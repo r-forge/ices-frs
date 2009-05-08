921b9e104b8ce77169b150b270384bfb990a500a
colinpmillar (Mr ) <colin.millar@jrc.ec.europa.eu>
2009-05-08 21:23:19 +0000

    Still testing!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ices-frs@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plots.index.corr.R
+++ pkg/R/plots.index.corr.R
@@ -1,5 +1,5 @@
 `plots.index.corr` <-
-function(object, main = NULL, cex = 1, tol = 0.05)
+function(object, main = NULL, cex = 1, tol = 0.05, file = "")
 {
   tune.mat <- object
   if (is.null(main)) main <- ""
@@ -10,6 +10,14 @@ function(object, main = NULL, cex = 1, tol = 0.05)
   for (j in 1:n) {
 	  cohort.mat[,j] <- c(rep(NA,n-j),tune.mat[,j],rep(NA,j-1))
   }
-  panel.pairs.cm (centre.log(cohort.mat), main = main, cex = cex, tol = tol)
+  if (file != "")
+  {
+    png(file, width=2000, height=2000, pointsize=36)
+      panel.pairs.cm (centre.log(cohort.mat), main = main, cex = cex*1.75, tol = tol)
+    dev.off()
+  } else
+  {
+    panel.pairs.cm (centre.log(cohort.mat), main = main, cex = cex, tol = tol)
+  }
 }
 
