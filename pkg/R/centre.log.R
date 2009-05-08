706b3c7ef434455cf7e90718f2556d4a471af378
colinpmillar (Mr ) <colin.millar@jrc.ec.europa.eu>
2009-05-08 15:54:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ices-frs@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/centre.log.R
+++
@@ -1,9 +0,0 @@
-`centre.log` <-
-function(mat)
-{
-  mat[mat<=0] <- NA
-  mat <- log(mat)
-  mat <- mat[,apply(mat,2,function(x) sum(!is.na(x))>2)]
-  apply(mat,2,function(x) (x-min(x, na.rm=TRUE))/diff(range(x,na.rm=TRUE)))
-}
-
--- pkg/R/diag.panel.cm.R
+++
@@ -1,6 +0,0 @@
-`diag.panel.cm` <-
-function (varname = NULL, cex = 1, ...)
-{
-  grid.text(varname, gp = gpar(col = "black", cex = cex))
-}
-
